6c17a0f2fd270f5c25c69b31252a3ae6ecf255bf
Kinjal <kinjal.j@like.digital>
2026-04-22 16:18:37 +0530

    added missing files

---
+++ numtalk-be/eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+
+  pluginReact.configs.flat.recommended,
+]);
