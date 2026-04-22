7da51e3a02c171ed3ca73af3e93bb3ac6e45feee
Kinjal <kinjal.j@like.digital>
2026-04-22 21:57:30 +0530

    tsconfig changes

--- numtalk-fe/src/shared/api/axios.ts
+++ numtalk-fe/src/shared/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.REACT_BASE_URL || "http://localhost:5000";
+const baseUrl = import.meta.env.REACT_BASE_URL || "https://num-talk-be.onrender.com";
 console.log(baseUrl)
 
 const api = axios.create({
